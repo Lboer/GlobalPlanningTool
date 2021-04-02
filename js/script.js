a585eb6055399626d2dca636983e21c81e609706
Luuk de Boer <s1131894@student.windesheim.nl>
2021-04-02 11:01:03 +0200

    All phases and products are now in json format, to do: format json to csv

--- js/script.js
+++ js/script.js
@@ -1,223 +1,235 @@
 let phaseSector = document.getElementById("phases");
 
 const planningObject = {
-    ID: 0,
-    Name: "phase",
-    Produce: "",
-    Status: "",
-    Start: new Date(),
-    End: new Date()
-}
+  Id: 0,
+  Name: "phase",
+  Produce: "",
+  Status: "",
+  Start: new Date(),
+  End: new Date(),
+};
 
 document.getElementById("addPhase").addEventListener("click", function () {
-    document.getElementsByClassName("hidden")[0].style.display = "block";
+  document.getElementsByClassName("hidden")[0].style.display = "block";
 
-    let div = document.createElement("div");
-    div.classList.add("phase");
+  let div = document.createElement("div");
+  div.classList.add("phase");
 
-    let number = document.getElementsByClassName("phase").length
-    let phaseNumber = (number + 1);
-    let phaseCode = document.createElement("h3");
-    phaseCode.classList.add("phaseNumber");
-    phaseCode.innerHTML = phaseNumber;
-    div.appendChild(phaseCode);
+  let number          = document.getElementsByClassName("phase").length;
+  let phaseNumber     = number + 1;
+  let phaseCode       = document.createElement("h3");
+  phaseCode.classList.add("phaseNumber");
+  phaseCode.innerHTML = phaseNumber;
+  div.appendChild(phaseCode);
 
-    let phaseName = document.createElement("input");
-    phaseName.type = "text";
-    phaseName.placeholder = "Insert phase name.";
-    phaseName.classList.add("phaseInput");
-    phaseName.classList.add("phaseName");
-    div.appendChild(phaseName);
+  let phaseName         = document.createElement("input");
+  phaseName.type        = "text";
+  phaseName.placeholder = "Insert phase name.";
+  phaseName.classList.add("phaseInput");
+  phaseName.classList.add("phaseName");
+  div.appendChild(phaseName);
 
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = "Delete this phase";
-    deleteBtn.classList.add("deletePhase");
-    div.appendChild(deleteBtn);
+  let deleteBtn       = document.createElement("button");
+  deleteBtn.innerHTML = "Delete this phase";
+  deleteBtn.classList.add("deletePhase");
+  div.appendChild(deleteBtn);
 
-    let breakline = document.createElement("div");
-    breakline.classList.add("phaseBR");
-    div.appendChild(breakline);
+  let breakline = document.createElement("div");
+  breakline.classList.add("phaseBR");
+  div.appendChild(breakline);
 
-    let startLabel = document.createElement("label");
-    startLabel.classList.add("dateLabel");
-    startLabel.innerHTML = "Start date";
-    div.appendChild(startLabel);
+  let startLabel = document.createElement("label");
+  startLabel.classList.add("dateLabel");
+  startLabel.innerHTML = "Start date";
+  div.appendChild(startLabel);
 
-    let endLabel = document.createElement("label");
-    endLabel.classList.add("dateLabel");
-    endLabel.innerHTML = "End date";
-    div.appendChild(endLabel);
+  let endLabel       = document.createElement("label");
+  endLabel.classList.add("dateLabel");
+  endLabel.innerHTML = "End date";
+  div.appendChild(endLabel);
 
-    div.appendChild(document.createElement("br"));
+  div.appendChild(document.createElement("br"));
 
-    let startDate = document.createElement("input");
-    startDate.type = "date";
-    startDate.classList.add("phaseInput");
-    startDate.classList.add("startDate");
-    div.appendChild(startDate);
+  let startDate  = document.createElement("input");
+  startDate.type = "date";
+  startDate.classList.add("phaseInput");
+  startDate.classList.add("startDate");
+  div.appendChild(startDate);
 
-    let addProduct = document.createElement("button");
-    addProduct.innerHTML = "Add a product";
-    addProduct.classList.add("addProduct");
-    div.appendChild(addProduct);
+  let addProduct       = document.createElement("button");
+  addProduct.innerHTML = "Add a product";
+  addProduct.classList.add("addProduct");
+  div.appendChild(addProduct);
 
-    let endDate = document.createElement("input");
-    endDate.type = "date";
-    endDate.classList.add("phaseInput");
-    endDate.classList.add("endDate");
-    div.appendChild(endDate);
+  let endDate  = document.createElement("input");
+  endDate.type = "date";
+  endDate.classList.add("phaseInput");
+  endDate.classList.add("endDate");
+  div.appendChild(endDate);
 
-    let break2 = document.createElement("div");
-    break2.classList.add("phaseBR");
-    div.appendChild(break2);
+  let break2 = document.createElement("div");
+  break2.classList.add("phaseBR");
+  div.appendChild(break2);
 
-    let status = document.createElement("select");
-    status.classList.add("status");
+  let status = document.createElement("select");
+  status.classList.add("status");
 
-    let placeholder = document.createElement("option");
-    placeholder.disabled = true;
-    placeholder.selected;
-    placeholder.innerHTML = "Select current phase";
-    status.appendChild(placeholder);
+  let placeholder       = document.createElement("option");
+  placeholder.disabled  = true;
+  placeholder.selected;
+  placeholder.innerHTML = "Select current phase";
+  status.appendChild(placeholder);
 
-    let statusBegin = document.createElement("option");
-    statusBegin.innerHTML = "To Initialise";
-    status.appendChild(statusBegin);
+  let statusBegin       = document.createElement("option");
+  statusBegin.innerHTML = "To Initialise";
+  status.appendChild(statusBegin);
 
-    let statusInProgress = document.createElement("option");
-    statusInProgress.innerHTML = "In Progress";
-    status.appendChild(statusInProgress);
+  let statusInProgress       = document.createElement("option");
+  statusInProgress.innerHTML = "In Progress";
+  status.appendChild(statusInProgress);
 
-    let statusFinished = document.createElement("option");
-    statusFinished.innerHTML = "Finished";
-    status.appendChild(statusFinished);
-    div.appendChild(status);
+  let statusFinished       = document.createElement("option");
+  statusFinished.innerHTML = "Finished";
+  status.appendChild(statusFinished);
+  div.appendChild(status);
 
-    div.appendChild(document.createElement("br"));
+  div.appendChild(document.createElement("br"));
 
-    let products = document.createElement("div");
-    products.classList.add("products");
-    div.appendChild(products);
-    
-    phaseSector.appendChild(div);
+  let products = document.createElement("div");
+  products.classList.add("products");
+  div.appendChild(products);
 
-    // Deleting the phase
-    let deleteBtnAction = document.getElementsByClassName("deletePhase");
+  phaseSector.appendChild(div);
 
-    for(let i = 0; i < deleteBtnAction.length; i++){
-        deleteBtnAction[i].addEventListener("click", function() {
-            let parent = this.parentNode;
-            parent.remove();
-        });
-    }
+  // Deleting the phase
+  let deleteBtnAction = document.getElementsByClassName("deletePhase");
 
-    // Adding products to the phase
-    let addBtnAction = document.getElementsByClassName("addProduct");
-    for(let i = addBtnAction.length - 1; i < addBtnAction.length; i++){
-        addBtnAction[i].addEventListener("click", function() {
-            let product = this.parentNode.children[13];
+  for (let i = 0; i < deleteBtnAction.length; i++) {
+    deleteBtnAction[i].addEventListener("click", function () {
+      let parent = this.parentNode;
+      parent.remove();
+    });
+  }
 
-            let label = document.createElement("label");
-            label.classList.add("productNumber");
-            label.innerHTML = productNumber(product);
-            product.appendChild(label);
+  // Adding products to the phase
+  let addBtnAction = document.getElementsByClassName("addProduct");
+  for (let i = addBtnAction.length - 1; i < addBtnAction.length; i++) {
+    addBtnAction[i].addEventListener("click", function () {
+      let product = this.parentNode.children[13];
 
-            let productName = document.createElement("input");
-            productName.type = "text";
-            productName.placeholder = "Product name...";
-            productName.classList.add("phaseInput");
-            productName.classList.add("phaseName");
-            product.appendChild(productName);
+      let label = document.createElement("label");
+      label.classList.add("productNumber");
+      label.innerHTML = productNumber(product);
+      product.appendChild(label);
 
-            product.appendChild(document.createElement("br"));
+      let productName         = document.createElement("input");
+      productName.type        = "text";
+      productName.placeholder = "Product name...";
+      productName.classList.add("phaseInput");
+      productName.classList.add("phaseName");
+      product.appendChild(productName);
 
-            let toDeliver = document.createElement("input");
-            toDeliver.type = "text";
-            toDeliver.placeholder = "Product to deliver";
-            toDeliver.classList.add("phaseInput");
-            toDeliver.classList.add("phaseName");
-            toDeliver.classList.add("toDeliver");
-            product.appendChild(toDeliver);
+      product.appendChild(document.createElement("br"));
 
-            let status = document.createElement("select");
-            status.classList.add("status");
+      let toDeliver         = document.createElement("input");
+      toDeliver.type        = "text";
+      toDeliver.placeholder = "Product to deliver";
+      toDeliver.classList.add("phaseInput");
+      toDeliver.classList.add("phaseName");
+      toDeliver.classList.add("toDeliver");
+      product.appendChild(toDeliver);
 
-            let placeholder = document.createElement("option");
-            placeholder.disabled = true;
-            placeholder.selected;
-            placeholder.innerHTML = "Select current phase";
-            status.appendChild(placeholder);
+      let status = document.createElement("select");
+      status.classList.add("status");
 
-            let statusBegin = document.createElement("option");
-            statusBegin.innerHTML = "To Initialise";
-            status.appendChild(statusBegin);
+      let placeholder       = document.createElement("option");
+      placeholder.disabled  = true;
+      placeholder.selected;
+      placeholder.innerHTML = "Select current phase";
+      status.appendChild(placeholder);
 
-            let statusInProgress = document.createElement("option");
-            statusInProgress.innerHTML = "In Progress";
-            status.appendChild(statusInProgress);
+      let statusBegin       = document.createElement("option");
+      statusBegin.innerHTML = "To Initialise";
+      status.appendChild(statusBegin);
 
-            let statusFinished = document.createElement("option");
-            statusFinished.innerHTML = "Finished";
-            status.appendChild(statusFinished);
-            product.appendChild(status);
+      let statusInProgress       = document.createElement("option");
+      statusInProgress.innerHTML = "In Progress";
+      status.appendChild(statusInProgress);
 
-            product.appendChild(document.createElement("br"));
+      let statusFinished       = document.createElement("option");
+      statusFinished.innerHTML = "Finished";
+      status.appendChild(statusFinished);
+      product.appendChild(status);
 
-            let startProductLabel = document.createElement("label");
-            startProductLabel.classList.add("dateLabel");
-            startProductLabel.innerHTML = "Start date";
-            product.appendChild(startProductLabel);
+      product.appendChild(document.createElement("br"));
 
-            let endProductLabel = document.createElement("label");
-            endProductLabel.classList.add("dateLabel");
-            endProductLabel.innerHTML = "End date";
-            product.appendChild(endProductLabel);
+      let startProductLabel       = document.createElement("label");
+      startProductLabel.classList.add("dateLabel");
+      startProductLabel.innerHTML = "Start date";
+      product.appendChild(startProductLabel);
 
-            product.appendChild(document.createElement("br"));
+      let endProductLabel       = document.createElement("label");
+      endProductLabel.classList.add("dateLabel");
+      endProductLabel.innerHTML = "End date";
+      product.appendChild(endProductLabel);
 
-            let startDate = document.createElement("input");
-            startDate.type = "date";
-            startDate.classList.add("phaseInput");
-            startDate.classList.add("startDate");
-            product.appendChild(startDate);
+      product.appendChild(document.createElement("br"));
 
-            let endDate = document.createElement("input");
-            endDate.type = "date";
-            endDate.classList.add("phaseInput");
-            endDate.classList.add("endDate");
-            product.appendChild(endDate);
+      let startDate  = document.createElement("input");
+      startDate.type = "date";
+      startDate.classList.add("phaseInput");
+      startDate.classList.add("startDate");
+      product.appendChild(startDate);
 
-            product.appendChild(document.createElement("br"));
+      let endDate  = document.createElement("input");
+      endDate.type = "date";
+      endDate.classList.add("phaseInput");
+      endDate.classList.add("endDate");
+      product.appendChild(endDate);
 
-            let break3 = document.createElement("div");
-            break3.classList.add("phaseBR");
-            div.appendChild(break3);
-        });
-    }
+      product.appendChild(document.createElement("br"));
+
+      let break3 = document.createElement("div");
+      break3.classList.add("phaseBR");
+      div.appendChild(break3);
+    });
+  }
 });
 
-function productNumber(parent)
-{
-    return `${parent.parentNode.children[0].lastChild.data[0]}.${parent.childElementCount/12}`;
+function productNumber(parent) {
+  return `${parent.parentNode.children[0].lastChild.data[0]}.${
+    1 + parent.childElementCount / 12
+  }`;
 }
 
 document.getElementById("export").addEventListener("click", function () {
-    let jsonArray = new Array();
-    for(i = 0; i < document.getElementsByClassName("phase").length; i++){
-        let phase = document.getElementsByClassName("phase")[i];
-        let object = Object.create(planningObject);
+  let jsonArray = new Array();
 
-        object.ID = parseFloat(phase.children[0].firstChild.data);
-        object.Name = phase.children[1].value;
-        object.Status = phase.children[11].value;
-        object.Start = phase.children[7].value;
-        object.End = phase.children[9].value;
+  for (i = 0; i < document.getElementsByClassName("phase").length; i++) {
+    let phase  = document.getElementsByClassName("phase")[i];
+    let object = Object.create(planningObject);
 
-        jsonArray.push(object);
-        for(i = 0; i < (phase.children[13].children.length/12); i++){
-            //add products to the array!
-        }
+    object.Id     = parseFloat(phase.children[0].innerHTML);
+    object.Name   = phase.children[1].value;
+    object.Status = phase.children[11].value;
+    object.Start  = phase.children[7].value;
+    object.End    = phase.children[9].value;
+
+    jsonArray.push(object);
+    let product = phase.children[13].children;
+    for (i = 0; i < product.length / 12; i++) {
+      let object     = Object.create(planningObject);
+      object.Id      = parseFloat(product[(i*12)+0].innerHTML);
+      object.Name    = product[(i*12)+1].value;
+      object.Produce = product[(i*12)+3].value;
+      object.Status  = product[(i*12)+4].value;
+      object.Start   = product[(i*12)+9].value;
+      object.End     = product[(i*12)+10].value;
+
+      jsonArray.push(object);
     }
-    console.log(jsonArray);
-    // ID, Name, To Deliver, Status, Starting Date, Ending Date
+  }
+
+  console.log(jsonArray);
+  // Convert jsonArray to .csv
 });
